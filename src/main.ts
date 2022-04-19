a5420ba10bbb7d1b4cf114ecacee8491c10ecd1a
Tim Perry <pimterry@gmail.com>
2022-04-19 20:44:20 +0200

    Allow locking IPFS rule matching to a given hostname

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import * as mockttp from 'mockttp';
-import { MockIpfsNode } from './mockipfs-node';
+import {
+    MockIPFSNode
+} from './mockipfs-node';
 
 export function getLocal(options?: mockttp.MockttpOptions) {
-    return new MockIpfsNode(mockttp.getLocal(options));
+    return new MockIPFSNode(mockttp.getLocal(options));
 }
 
 export function getRemote(options?: mockttp.MockttpClientOptions) {
-    return new MockIpfsNode(mockttp.getRemote(options));
+    return new MockIPFSNode(mockttp.getRemote(options));
 }
 
 export function getStandalone(options?: mockttp.StandaloneServerOptions) {
--- src/mockipfs-node.ts
+++ src/mockipfs-node.ts
@@ -2,10 +2,32 @@ import { Mockttp } from "mockttp";
 
 import { RuleBuilder } from "./rule-builder";
 
-export class MockIpfsNode {
+export interface MockIPFSOptions {
+    /**
+     * The hostname that will be used for the IPFS node. This can be any
+     * DNS or IP address that resolves to this node.
+     *
+     * If set, this hostname will be returned in ipfsOptions() and the
+     * mock IPFS behaviour will apply only to requests explicitly sent
+     * to this hostname.
+     *
+     * This can be useful when accessing the IPFS mock node remotely, or
+     * when using this alongside Mockttp, to ensure that IPFS mocks do
+     * not interfere with other HTTP traffic mocked elsewhere.
+     */
+    nodeHostname?: string;
+};
+
+export class MockIPFSNode {
+
+    private readonly hostname: string | undefined;
+
     constructor(
-        private mockttpServer: Mockttp
-    ) {}
+        private mockttpServer: Mockttp,
+        options: MockIPFSOptions = {}
+    ) {
+        this.hostname = options.nodeHostname;
+    }
 
     start = () => this.mockttpServer.start();
     stop = () => this.mockttpServer.stop();
@@ -13,13 +35,17 @@ export class MockIpfsNode {
     get ipfsOptions() {
         return {
             protocol: 'http',
-            host: '127.0.0.1',
+            host: this.hostname,
             port: this.mockttpServer.port
         };
     }
 
     forCat(ipfsPath?: string) {
-        let catRuleBuilder = this.mockttpServer.post('/api/v0/cat');
+        let catRuleBuilder = this.mockttpServer.forPost('/api/v0/cat')
+
+        if (this.hostname) {
+            catRuleBuilder = catRuleBuilder.forHostname(this.hostname);
+        }
 
         if (ipfsPath !== undefined) {
             catRuleBuilder = catRuleBuilder.withQuery({ arg: ipfsPath });
