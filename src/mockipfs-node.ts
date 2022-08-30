877cee7c1bb09305c333aa74171e85ffb803c331
Tim Perry <pimterry@gmail.com>
2022-08-30 14:06:01 +0200

    Add JSDoc for each of the MockIPFS node methods

--- src/mockipfs-node.ts
+++ src/mockipfs-node.ts
@@ -20,11 +20,36 @@ import { PinRmRuleBuilder } from "./pinning/pin-rm-rule-builder";
 import { PinLsRuleBuilder } from "./pinning/pin-ls-rule-builder";
 
 export interface MockIPFSOptions {
+    /**
+     * Specify the behaviour of unmatched requests.
+     *
+     * By default this is set to `stub`, in which case default responses will be
+     * returned, emulating a constantly available but empty IPFS node: all queries
+     * will return no data, and all submitted data will be accepted (but ignored).
+     *
+     * Alternatively, this can be set to an object including a `proxyTo` property,
+     * defining the URL of a remote IPFS node to which unmatched requests should be
+     * forwarded. In this case all default behaviours will be disabled, and all
+     * unmatched requests will receive real responses from that upstream IPFS node.
+     */
     unmatchedRequests?:
         | 'stub'
         | { proxyTo: string }
 }
 
+/**
+ * A MockIPFS node provides default behaviours and allows defining custom behaviour
+ * rules to simulate interactions with the IPFS network without requiring a full
+ * node or access to the real IPFS network.
+ *
+ * This should not be created directly: instead, call then `getLocal()` or `getRemote()`
+ * methods exported from this module.
+ *
+ * Once you have a MockIPFS node, you can start defining rules using any of the
+ * `forX()` methods. Each method returns a rule builder, allowing you to add extra
+ * matching constraints, followed by a `thenX()` final method which enables the rule,
+ * returning a promise that resolves once the rule is constructed and active.
+ */
 export class MockIPFSNode {
 
     private ipnsMock: IPNSMock;
@@ -45,6 +70,11 @@ export class MockIPFSNode {
         this.catMock = new CatMock(this.mockttpServer);
     }
 
+    /**
+     * The node must be started before use. Starting the node resets it, removing any
+     * rules that may have been added previously and configuring default behaviours
+     * for unmatched requests.
+     */
     async start() {
         this.reset();
         await this.mockttpServer.start();
@@ -60,6 +90,11 @@ export class MockIPFSNode {
         this.mockttpServer.reset();
     }
 
+    /**
+     * The IPFS options required to connect to this MockIPFS node. These can be passed
+     * directly to `IPFS.create` from ipfs-http-client to create a real IPFS client
+     * that connects to this mock node.
+     */
     get ipfsOptions() {
         return {
             protocol: 'http',
@@ -92,6 +127,13 @@ export class MockIPFSNode {
         }
     }
 
+    /**
+     * Mock IPFS cat requests, returning fake content instead of the
+     * real content for the given CID.
+     *
+     * This takes an optional CID argument. If not provided, the mock
+     * will match all cat requests for any CID.
+     */
     forCat(cid?: string) {
         let catRuleBuilder = this.mockttpServer.forPost('/api/v0/cat');
 
@@ -102,10 +144,20 @@ export class MockIPFSNode {
         return new CatRuleBuilder(catRuleBuilder);
     }
 
+    /**
+     * Mock IPFS add requests, mocking the behaviour of the add command
+     * while ensuring that the added content is never actually sent
+     * to a real IPFS node.
+     */
     forAdd() {
         return this.forAddIncluding();
     }
 
+    /**
+     * Mock IPFS add requests containing certain types of content, mocking
+     * the behaviour of the add command while ensuring that the added content
+     * is never actually sent to a real IPFS node.
+     */
     forAddIncluding(...content: Array<
         | string
         | Uint8Array
@@ -117,6 +169,12 @@ export class MockIPFSNode {
         );
     }
 
+    /**
+     * Mock the behaviour of IPNS name resolutions.
+     *
+     * This takes an optional name argument to match. If not provided, the
+     * defined behaviour will apply for all IPFS resolutions for any name.
+     */
     forNameResolve(name?: string) {
         return new NameResolveRuleBuilder(
             name,
@@ -124,6 +182,12 @@ export class MockIPFSNode {
         );
     }
 
+    /**
+     * Mock the behaviour of IPNS name publishing.
+     *
+     * This takes an optional name argument to match. If not provided, the
+     * defined behaviour will apply for all IPFS resolutions for any name.
+     */
     forNamePublish(nameKey?: string) {
         return new NamePublishRuleBuilder(
             nameKey,
@@ -131,6 +195,12 @@ export class MockIPFSNode {
         );
     }
 
+    /**
+     * Mock the behaviour of IPFS pinning.
+     *
+     * This takes an optional CID argument. If not provided, the mock
+     * will match pinning of any CID.
+     */
     forPinAdd(cid?: string) {
         return new PinAddRuleBuilder(
             cid,
@@ -138,6 +208,12 @@ export class MockIPFSNode {
         );
     }
 
+    /**
+     * Mock the behaviour of IPFS pin removal.
+     *
+     * This takes an optional CID argument. If not provided, the mock
+     * will match unpinning of any CID.
+     */
     forPinRm(cid?: string) {
         return new PinRmRuleBuilder(
             cid,
@@ -145,32 +221,59 @@ export class MockIPFSNode {
         );
     }
 
+    /**
+     * Mock the behaviour of IPFS pin listing.
+     */
     forPinLs() {
         return new PinLsRuleBuilder(
             this.pinningMock.addPinLsRule
         );
     }
 
+    /**
+     * Get the list of all IPFS CIDs that have been requested
+     * from this IPFS mock node since it started.
+     */
     async getQueriedContent(): Promise<Array<{ path: string }>> {
         return this.catMock.getQueriedContent(this.seenRequests);
     }
 
+    /**
+     * Get the list of all IPFS CIDs & content that have been added to
+     * this IPFS mock node since it started.
+     */
     async getAddedContent(): Promise<Array<{ path?: string, content?: Uint8Array }>> {
         return this.addMock.getAddedContent(this.seenRequests);
     }
 
+    /**
+     * Get the list of all IPNS resolution queries that have been received by
+     * this IPFS mock node since it started.
+     */
     async getIPNSQueries(): Promise<Array<{ name: string | null }>> {
         return this.ipnsMock.getIPNSQueries(this.seenRequests);
     }
 
+    /**
+     * Get the list of all IPNS publications that have been received by
+     * this IPFS mock node since it started.
+     */
     async getIPNSPublications(): Promise<Array<{ name: string | null, value: string }>> {
         return this.ipnsMock.getIPNSPublications(this.seenRequests);
     }
 
+    /**
+     * Get the list of all IPFS pins that have been added to this IPFS mock
+     * node since it started.
+     */
     async getAddedPins(): Promise<Array<{ cid: string }>> {
         return this.pinningMock.getAddedPins(this.seenRequests);
     }
 
+    /**
+     * Get the list of all IPFS pins that have been removed from this IPFS mock
+     * node since it started.
+     */
     async getRemovedPins(): Promise<Array<{ cid: string }>> {
         return this.pinningMock.getRemovedPins(this.seenRequests);
     }
